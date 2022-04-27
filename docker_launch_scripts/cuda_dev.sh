ad78fcea00476c1e4d53ff5e5a53091a8019e449
perseusdg <f20180523@goa.bits-pilani.ac.in>
2022-04-27 18:15:55 +0530

    added launch scripts and support for jupyter notebook in pytorch docker

---
+++ docker_launch_scripts/cuda_dev.sh
@@ -0,0 +1,14 @@
+xhost local:root
+docker run --rm -it -d --runtime=nvidia --privileged --net=host --ipc=host \
+-v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
+-v $HOME/.Xauthority:/home/$(id -un)/.Xauthority -e XAUTHORITY=/home/$(id -un)/.Xauthority \
+-e DOCKER_USER_NAME=$(id -un) \
+-e DOCKER_USER_ID=$(id -u) \
+-e DOCKER_USER_GROUP_NAME=$(id -gn) \
+-e DOCKER_USER_GROUP_ID=$(id -g) \
+-v $HOME/.ssh:/home/$(id -un)/.ssh \
+-v $HOME/.vscode:/home/$(id -un)/.vscode \
+-v $HOME/.config/Code/User/settings.json:/home/$(id -un)/.config/Code/User/settings.json \
+-v $HOME/Datasets:/home/$(id -un)/Datasets \
+-v $HOME/docker_development/cuda_dev:/home/$(id -un)/Development \
+perseusdg/cuda_dev:opencv4.5
---
+++ install_launch_scripts.sh
@@ -0,0 +1,3 @@
+cp -r docker_launch_scripts ~/.docker
+mkdir ~/docker_development/cuda_dev
+mkdir ~/docker_development/pytorch_dev
